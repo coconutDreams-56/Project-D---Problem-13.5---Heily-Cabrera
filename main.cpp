c4117f71a18c53329ebc3f6eee91fb8ac12e73f3
coconutDreams-56 <cabreraheily@gmail.com>
2025-07-11 18:03:24 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
+//# Project-D---Problem-13.5---Heily-Cabrera
 
+// Define a class Set that stores a finite set of integers. (In a set, the order of elements does not matter, and every element can occur at most once.) Supply add and remove member functions to add and remove set elements. Overload the | and & operators to compute the union and intersection of the set, and the << operator to send the set contents to a stream // 
+
+//Programmer: Heily Cabrera
+//Tester: Audrey Jacobson 
+
+// Group Project C //
+/***** Members *****/
+/***** Ashley Jacobson *****/
+/***** Giannfranco Brance *****/
+/***** Audrey Tapia *****/
+/***** Heily Cabrera Guerrero *****/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
